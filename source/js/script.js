7fe1efea4ab653e571abc8f1e2e4ed5370a56f5e
Дмитрий <gruneiter@gmail.com>
2021-09-26 19:57:38 +0300

    Реализовываем анимацию иллюстрации утешительного приза

--- source/js/script.js
+++ source/js/script.js
@@ -24,7 +24,7 @@ form();
 social();
 body();
 rules();
-prizeAnimation(`primaryAwardBegin`, `secondaryAwardBegin`);
+prizeAnimation(`primaryAwardBegin`, `secondaryAwardBegin`, `additionalAwardBegin`);
 
 const fullPageScroll = new FullPageScroll();
 fullPageScroll.init();
